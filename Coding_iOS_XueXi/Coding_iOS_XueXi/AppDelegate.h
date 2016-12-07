2a6a04baa3080a27a8f5df21cafe729266f2a5f3
Yinjianhua472392556 <18620526218@163.com>
2016-12-07 09:39:04 +0800

    初始化项目，使用Cocopods管理三方库

---
+++ Coding_iOS_XueXi/Coding_iOS_XueXi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Coding_iOS_XueXi
+//
+//  Created by apple on 16/12/7.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Coding_iOS_XueXi/Coding_iOS_XueXi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Coding_iOS_XueXi
+//
+//  Created by apple on 16/12/7.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
